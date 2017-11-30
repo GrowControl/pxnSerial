d4146341a84a4c96615b345b8a79f7aab92c1606
lorenzop <lorenzo@poixson.com>
2017-11-30 02:25:59 -0500

    added pxnSerial and pxnSerialChannel classes (mostly finished!)

---
+++ core/src/com/poixson/serial/pxnSerial.java
@@ -0,0 +1,408 @@
+package com.poixson.serial;
+
+import java.io.IOException;
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.poixson.serial.enums.SerialState;
+import com.poixson.serial.natives.NativeSerial;
+import com.poixson.utils.StringUtils;
+import com.poixson.utils.Utils;
+import com.poixson.utils.xCloseable;
+import com.poixson.utils.exceptions.RequiredArgumentException;
+
+
+public class pxnSerial implements xCloseable {
+
+	public static final int DEFAULT_STRING_BUFFER_SIZE = 4096;
+
+	public final SerialConfig cfg;
+	public final DeviceNative nat;
+
+	protected volatile int    handle   = 0;
+	protected volatile String errorMsg = null;
+
+	protected final ReentrantLock stateLock = new ReentrantLock();
+	protected final ReentrantLock readLock  = new ReentrantLock();
+	protected final ReentrantLock writeLock = new ReentrantLock();
+
+	private byte[] tmpSingleReadByte  = new byte[1];
+	private byte[] tmpSingleWriteByte = new byte[1];
+
+
+
+	public pxnSerial(final SerialConfig cfg) {
+		this(
+			cfg,
+			new NativeSerial()
+		);
+	}
+	public pxnSerial(final SerialConfig cfg, final DeviceNative nat) {
+		if (cfg == null) throw new RequiredArgumentException("cfg");
+		if (nat == null) throw new RequiredArgumentException("nat");
+		this.cfg = cfg.clone().lock();
+		this.nat = nat;
+	}
+	public void finalize() {
+		Utils.safeClose(this);
+		this.nat.unload();
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// open/close port
+
+
+
+	@SuppressWarnings("resource")
+	public void open() throws IOException {
+		if ( ! this.stateLock.tryLock()) {
+			throw new IOException("Already opening port "+this.getPortName());
+		}
+		try {
+			// check port state
+			switch (this.getState()) {
+			case OPEN:
+				throw new IOException("Port already open "+this.getPortName());
+			case ERROR:
+				Utils.safeClose(this);
+			default:
+			}
+			// open the port
+			final int h =
+				this.nat.openPort(
+					this.getPortName()
+				);
+			// check state
+			{
+				final SerialState state = SerialState.valueOf(h);
+				switch (state) {
+				case ERROR:
+				case CLOSED:
+					this.handle = h;
+					final String errMsg = this.getErrorMsg();
+					throw new IOException(
+						(new StringBuilder())
+							.append("Failed to open port ")
+							.append(this.getPortName())
+							.append(" (h:").append(h)
+							.append(
+								Utils.isEmpty(errMsg)
+								? ")"
+								: ") "+errMsg
+							)
+							.toString()
+					);
+				case OPEN:
+				default:
+				}
+			}
+			// configure the port
+			{
+				final int result =
+					this.nat.setParams(
+						h,
+						this.cfg.getBaudValue(),
+						this.cfg.getByteSizeValue(),
+						this.cfg.getStopBitsValue(),
+						this.cfg.getParityValue(),
+						this.cfg.getFlags()
+					);
+				// failed to set params
+				if (result <= 0) {
+					Utils.safeClose(this);
+					this.handle = result;
+					throw new IOException(
+						(new StringBuilder())
+							.append("Failed to configure serial port: ")
+							.append(result)
+							.append(" ")
+							.append(this.getPortName())
+							.toString()
+					);
+				}
+			}
+			// set line status
+			{
+				final int result =
+					this.nat.setLineStatus(
+						h,
+						this.cfg.getRTS(),
+						this.cfg.getDTR()
+					);
+				// failed to set line status
+				if (result <= 0) {
+					this.handle = result;
+					throw new IOException(
+						(new StringBuilder())
+							.append("Failed to set serial line status: ")
+							.append(result)
+							.append(" ")
+							.append(this.getPortName())
+							.toString()
+					);
+				}
+			}
+			this.handle = h;
+		} finally {
+			this.stateLock.unlock();
+		}
+	}
+
+
+
+	@Override
+	public void close() throws IOException {
+		this.ensureOpenAndLock();
+		try {
+			if (this.isOpen()) {
+				this.flush();
+			}
+			this.nat.closePort(this.handle);
+			this.handle   = 0;
+			this.errorMsg = null;
+		} finally {
+			this.stateLock.unlock();
+		}
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// port state
+
+
+
+	public SerialState getState() {
+		return
+			SerialState.valueOf(
+				this.getHandle()
+			);
+	}
+	public int getHandle() {
+		this.stateLock.lock();
+		try {
+			return this.handle;
+		} finally {
+			this.stateLock.unlock();
+		}
+	}
+
+
+
+	public boolean isOpen() {
+		return
+			SerialState.OPEN.equals(
+				this.getState()
+			);
+	}
+	@Override
+	public boolean isClosed() {
+		return
+			SerialState.CLOSED.equals(
+				this.getState()
+			);
+	}
+
+
+
+	protected void ensureOpenAndLock() throws IOException {
+		this.stateLock.lock();
+		if ( ! this.isOpen()) {
+			this.stateLock.unlock();
+			throw new IOException("Port is not open "+this.getPortName());
+		}
+	}
+
+
+
+	public String getErrorMsg() {
+		return this.errorMsg;
+	}
+	public void setErrorMsg(final String errMsg) {
+		this.errorMsg = errMsg;
+	}
+
+
+
+	public SerialConfig getConfig() {
+		return this.cfg;
+	}
+	public DeviceNative getNative() {
+		return this.nat;
+	}
+
+
+
+	public String getPortName() {
+		return this.cfg.portName;
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// read
+
+
+
+	public int available() {
+		try {
+			this.ensureOpenAndLock();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return -1;
+		}
+		try {
+			return this.nat.available(this.handle);
+		} finally {
+			this.stateLock.unlock();
+		}
+	}
+	public boolean hasAvailable() {
+		return (this.available() > 0);
+	}
+
+
+
+	public int read() throws IOException {
+		final int result =
+			this.read(
+				this.tmpSingleReadByte,
+				0,
+				1
+			);
+		if (result < 0) throw new IOException();
+		return this.tmpSingleReadByte[1];
+	}
+	public int read(byte bytes[]) throws IOException {
+		return
+			this.read(
+				bytes,
+				0,
+				bytes.length
+			);
+	}
+	public int read(byte[] bytes, final int len) throws IOException {
+		return
+			this.read(
+				bytes,
+				0,
+				len
+			);
+	}
+	public int read(byte[] bytes, final int offset, final int len) throws IOException {
+		if (bytes == null) throw new NullPointerException();
+		if (offset < 0) throw new IllegalArgumentException("offset cannot be less than 0");
+		if (len    < 1) throw new IllegalArgumentException("len cannot be less than 1");
+		if (bytes.length < offset + len) throw new IllegalArgumentException("offset/len out of range");
+		this.ensureOpenAndLock();
+		try {
+			this.readLock.lock();
+			try {
+//TODO: add the offset argument
+				return
+					this.nat.readBytes(
+						this.handle,
+						bytes,
+						len
+					);
+			} finally {
+				this.readLock.unlock();
+			}
+		} finally {
+			this.stateLock.unlock();
+		}
+	}
+
+
+
+	public String readString() throws IOException {
+		return this.readString(DEFAULT_STRING_BUFFER_SIZE);
+	}
+	public String readString(final int maxLength) throws IOException {
+		byte[] bytes = new byte[maxLength];
+		final int result =
+			this.read(
+				bytes,
+				0,
+				maxLength
+			);
+		if (result < 0) throw new IOException();
+		if (result == 0)
+			return "";
+		return
+			new String(
+				bytes,
+				0,      // offset
+				result, // length
+				StringUtils.UTF8
+			);
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// write
+
+
+
+	public void flush() throws IOException {
+//TODO:
+//		this.nat.flush();
+	}
+
+
+
+	public void write(final int b) throws IOException {
+		this.tmpSingleWriteByte[0] = (byte) b;
+		this.write(
+			this.tmpSingleWriteByte,
+			0,
+			1
+		);
+	}
+	public void write(final byte[] bytes) throws IOException {
+		this.write(
+			bytes,
+			0,
+			bytes.length
+		);
+	}
+	public void write(final byte[] bytes, final int len) throws IOException {
+		this.write(
+			bytes,
+			0,
+			len
+		);
+	}
+	public void write(final byte[] bytes, final int offset, final int len) throws IOException {
+		if (bytes == null) throw new NullPointerException();
+		if (offset < 0) throw new IllegalArgumentException("offset cannot be less than 0");
+		if (len    < 1) throw new IllegalArgumentException("len cannot be less than 1");
+		if (bytes.length < offset + len) throw new IllegalArgumentException("offset/len out of range");
+		this.ensureOpenAndLock();
+		try {
+			this.writeLock.lock();
+			try {
+//TODO: add the offset and len arguments
+				this.nat.writeBytes(this.handle, bytes);
+			} finally {
+				this.writeLock.unlock();
+			}
+		} finally {
+			this.stateLock.unlock();
+		}
+	}
+
+
+
+	public void writeString(final String msg) throws IOException {
+		if (Utils.isEmpty(msg)) throw new NullPointerException();
+		final byte[] bytes =
+			msg.getBytes(StringUtils.UTF8);
+		this.write(bytes);
+	}
+
+
+
+}
---
+++ core/src/com/poixson/serial/pxnSerialChannel.java
@@ -0,0 +1,265 @@
+package com.poixson.serial;
+
+import static java.nio.channels.SelectionKey.OP_READ;
+import static java.nio.channels.SelectionKey.OP_WRITE;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.spi.AbstractSelectableChannel;
+
+import com.poixson.serial.enums.Baud;
+import com.poixson.utils.Utils;
+import com.poixson.utils.exceptions.RequiredArgumentException;
+
+import sun.nio.ch.IOUtil;
+import sun.nio.ch.Net;
+import sun.nio.ch.SelChImpl;
+import sun.nio.ch.SelectionKeyImpl;
+
+
+public class pxnSerialChannel extends AbstractSelectableChannel implements SelChImpl {
+
+	private static final int VALID_OPS = (OP_READ | OP_WRITE);
+
+	protected final pxnSerial serial;
+
+	protected final FileDescriptor fd;
+	protected final int            handle;
+
+
+
+	public static pxnSerialChannel open(final String portName, final int baudInt) throws IOException {
+		final Baud baud = Baud.FromInt(baudInt);
+		if (baud == null) throw new IllegalArgumentException("Invalid baud: "+Integer.toString(baudInt));
+		return open(
+			portName,
+			baud
+		);
+	}
+	public static pxnSerialChannel open(final String portName, final Baud baud) throws IOException {
+		if (Utils.isEmpty(portName)) throw new RequiredArgumentException("portName");
+		if (baud == null)            throw new RequiredArgumentException("baud");
+		final SerialConfig cfg =
+			(new SerialConfig())
+				.setPortName(portName)
+				.setBaud(baud);
+		final pxnSerial serial =
+			new pxnSerial(cfg);
+		return open(serial);
+	}
+	public static pxnSerialChannel open(final pxnSerial serial)
+			throws IOException {
+		return new pxnSerialChannel(serial);
+	}
+
+
+
+	protected pxnSerialChannel(final pxnSerial serial) throws IOException {
+		super(null);
+		if (serial == null) throw new RequiredArgumentException("serial");
+		this.serial = serial;
+		// ensure port is open
+		if ( ! serial.isOpen()) {
+			serial.open();
+		}
+		this.handle = serial.getHandle();
+		this.fd     = IOUtil.newFD(this.handle);
+	}
+
+
+
+	public pxnSerial serial() {
+		return this.serial;
+	}
+
+
+
+	@Override
+	public FileDescriptor getFD() {
+		return this.fd;
+	}
+	@Override
+	public int getFDVal() {
+		return this.handle;
+	}
+
+
+
+	// interrupt thread
+	@Override
+	public void kill() throws IOException {
+//TODO: is this needed?
+	}
+	@Override
+	protected void implCloseSelectableChannel() throws IOException {
+		this.serial
+			.close();
+	}
+
+
+
+	@Override
+	protected void implConfigureBlocking(final boolean block) throws IOException {
+//TODO:
+//		this.serial.setBlocking(blocking);
+	}
+
+
+
+	@Override
+	public int validOps() {
+		return VALID_OPS;
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// read
+
+
+
+	public int available() {
+		return this.serial
+				.available();
+	}
+	public boolean hasAvailable() {
+		return this.serial
+				.hasAvailable();
+	}
+
+
+
+	public int read() throws IOException {
+		return this.serial
+				.read();
+	}
+	public int read(byte bytes[]) throws IOException {
+		return this.serial
+				.read(bytes);
+	}
+	public int read(byte[] bytes, final int len) throws IOException {
+		return this.serial
+				.read(bytes, len);
+	}
+	public int read(byte[] bytes, final int offset, final int len) throws IOException {
+		return this.serial
+				.read(bytes, offset, len);
+	}
+
+
+
+	public String readString() throws IOException {
+		return this.serial
+				.readString();
+	}
+	public String readString(final int maxLength) throws IOException {
+		return this.serial
+				.readString(maxLength);
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// write
+
+
+
+	public void flush() throws IOException {
+		this.serial
+			.flush();
+	}
+
+
+
+	public void write(final int b) throws IOException {
+		this.serial
+			.write(b);
+	}
+	public void write(final byte[] bytes) throws IOException {
+		this.serial
+			.write(bytes);
+	}
+	public void write(final byte[] bytes, final int len) throws IOException {
+		this.serial
+			.write(bytes, len);
+	}
+	public void write(final byte[] bytes, final int offset, final int len) throws IOException {
+		this.serial
+			.write(bytes, offset, len);
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+
+
+
+	@Override
+	public boolean translateAndUpdateReadyOps(final int options, final SelectionKeyImpl skey) {
+		return
+			translateReadyOps(
+				options,
+				skey.nioReadyOps(),
+				skey
+			);
+	}
+	@Override
+	public boolean translateAndSetReadyOps(final int options, final SelectionKeyImpl skey) {
+		return
+			translateReadyOps(
+				options,
+				0,
+				skey
+			);
+	}
+	@Override
+	public void translateAndSetInterestOps(final int options, final SelectionKeyImpl skey) {
+		int newOps = 0;
+		if ( 0 != (options & SelectionKey.OP_READ) )
+			newOps |= Net.POLLIN;
+		if ( 0 != (options & SelectionKey.OP_WRITE) )
+			newOps |= Net.POLLOUT;
+//		if ( 0 != (options & SelectionKey.OP_CONNECT) )
+//			newOps |= Net.POLLCONN;
+		skey.selector.putEventOps(skey, newOps);
+	}
+	public boolean translateReadyOps(final int options, final int initialOps, final SelectionKeyImpl sKey) {
+		if ( (options & Net.POLLNVAL) != 0)
+			return false;
+		int intOptions = sKey.nioInterestOps();
+		int oldOptions = sKey.nioReadyOps();
+		int newOptions = initialOps;
+		if ((options & (Net.POLLERR | Net.POLLHUP)) != 0) {
+			newOptions = intOptions;
+			sKey.nioReadyOps(newOptions);
+			return ((newOptions & ~oldOptions) != 0);
+		}
+		if ( (options & Net.POLLIN) != 0) {
+			if ( (intOptions & SelectionKey.OP_READ) != 0) {
+				if (this.isOpen()) {
+					newOptions |= SelectionKey.OP_READ;
+				}
+			}
+		}
+		if ( (options & Net.POLLOUT) != 0) {
+			if ( (intOptions & SelectionKey.OP_WRITE) != 0) {
+				if (this.isOpen()) {
+					newOptions |= SelectionKey.OP_WRITE;
+				}
+			}
+		}
+		sKey.nioReadyOps(newOptions);
+		return ( (newOptions & ~oldOptions) != 0);
+	}
+
+
+
+	public void writeString(final String msg) throws IOException {
+		this.serial
+			.writeString(msg);
+	}
+
+
+
+}
