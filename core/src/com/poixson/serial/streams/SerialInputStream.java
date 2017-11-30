81f3da5f7ce464997e70b7827b4c8da1f46f22fb
lorenzop <lorenzo@poixson.com>
2017-11-29 23:00:09 -0500

    added SerialInputStream and SerialOutputStream classes

---
+++ core/src/com/poixson/serial/streams/SerialInputStream.java
@@ -0,0 +1,63 @@
+package com.poixson.serial.streams;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.poixson.serial.pxnSerial;
+import com.poixson.utils.exceptions.RequiredArgumentException;
+
+
+public class SerialInputStream extends InputStream {
+
+	private final pxnSerial serial;
+
+
+
+	public SerialInputStream(final pxnSerial serial) {
+		if (serial == null) throw new RequiredArgumentException("serial");
+		this.serial = serial;
+	}
+
+
+
+	@Override
+	public void close() throws IOException {
+		try {
+			super.close();
+		} finally {
+			this.serial.close();
+		}
+	}
+
+
+
+	@Override
+	public int available() {
+		return this.serial.available();
+	}
+
+
+
+	@Override
+	public int read() throws IOException {
+		return this.serial
+			.read();
+	}
+	@Override
+	public int read(byte bytes[]) throws IOException {
+		return this.serial
+			.read(bytes);
+	}
+	public int read(byte[] bytes, final int len) throws IOException {
+		return this.serial
+			.read(bytes, len);
+	}
+	@Override
+	public int read(byte[] bytes, final int offset, final int len) throws IOException {
+		return this.serial
+			.read(bytes, offset, len);
+	}
+
+
+
+}
---
+++ core/src/com/poixson/serial/streams/SerialOutputStream.java
@@ -0,0 +1,62 @@
+package com.poixson.serial.streams;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.poixson.serial.pxnSerial;
+import com.poixson.utils.exceptions.RequiredArgumentException;
+
+
+public class SerialOutputStream extends OutputStream {
+
+	private final pxnSerial serial;
+
+
+
+	public SerialOutputStream(final pxnSerial serial) {
+		if (serial == null) throw new RequiredArgumentException("serial");
+		this.serial = serial;
+	}
+
+
+
+	@Override
+	public void close() throws IOException {
+		try {
+			super.close();
+		} finally {
+			this.serial.close();
+		}
+	}
+
+
+
+	@Override
+	public void flush() throws IOException {
+	}
+
+
+
+	@Override
+	public void write(final int b) throws IOException {
+		this.serial
+			.write(b);
+	}
+	@Override
+	public void write(final byte[] bytes) throws IOException {
+		this.serial
+			.write(bytes);
+	}
+	public void write(final byte[] bytes, final int len) throws IOException {
+		this.serial
+			.write(bytes, len);
+	}
+	@Override
+	public void write(final byte[] bytes, final int offset, final int len) throws IOException {
+		this.serial
+			.write(bytes, offset, len);
+	}
+
+
+
+}
